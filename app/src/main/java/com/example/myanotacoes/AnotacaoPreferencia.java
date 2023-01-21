ab8dadce8f03b1ef4f31875072e8cdc1b7b253d0
sfarinas <sfarinas@gmail.com>
2023-01-21 10:30:43 -0300

    SEGUNDO COMMIT

---
+++ app/src/main/java/com/example/myanotacoes/AnotacaoPreferencia.java
@@ -0,0 +1,32 @@
+package com.example.myanotacoes;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class AnotacaoPreferencia {
+
+    private Context context;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private final String NOME_ARQUIVO = "anotacao.preferencias";
+    private final String CHAVE_NOME = "nome";
+
+    public AnotacaoPreferencia(Context c) {
+        this.context = c;
+        preferences = context.getSharedPreferences(NOME_ARQUIVO, 0);
+        editor = preferences.edit();
+
+    }
+
+    public void salvarAnotacao(String anotacao){
+        editor.putString(CHAVE_NOME, anotacao);
+        editor.commit();
+
+    }
+
+    public String recuperarAnotacao(){
+
+        return preferences.getString(CHAVE_NOME, "");
+    }
+
+}
--- app/src/main/java/com/example/myanotacoes/MainActivity.java
+++ app/src/main/java/com/example/myanotacoes/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.myanotacoes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AnotacaoPreferencia preferencia;
+    private EditText editAnotacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferencia = new AnotacaoPreferencia( getApplicationContext() );
+
+        FloatingActionButton BotaoSalvar = findViewById(R.id.BotaoSalvar);
+        editAnotacao = findViewById(R.id.editAnotacao);
+        BotaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Validar entrada de dados
+                String textoRecuperado = editAnotacao.getText().toString();
+                if ( textoRecuperado.equals("") ){
+                    Snackbar.make(view, "Digite sua primeira Anotação", Snackbar.LENGTH_LONG).show();
+                }else {
+                    preferencia.salvarAnotacao( textoRecuperado );
+                    Snackbar.make(view, "Anotação Salva com Sucesso", Snackbar.LENGTH_LONG).show();
+                }
+
+                //Snackbar.make(view, "Preencha a anotação", Snackbar.LENGTH_LONG).show();
+            }
+        });
+        //Recuperar Anotacao
+        String anotacao = preferencia.recuperarAnotacao();
+        if ( !anotacao.equals("") ){
+            editAnotacao.setText( anotacao );
+        }
     }
 }
